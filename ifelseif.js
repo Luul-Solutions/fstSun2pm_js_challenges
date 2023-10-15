3022e9934174669bbe47accb959f1ebbcc54e0c2
= <=>
2023-10-15 15:51:15 +0100

    added if else if chained example

---
+++ ifelseif.js
@@ -0,0 +1,11 @@
+// use if and else chained to print what I do Saturday, Sunday and Monday
+let today = "Saturday"
+if (today == "Saturday") {
+    console.log("Come to the coding school")
+} else if (today == "Sunday") {
+    console.log("Sleep")
+} else if (today == "Monday") {
+    console.log("go to school")
+} else {
+    console.log("Do whatever you like")
+}
