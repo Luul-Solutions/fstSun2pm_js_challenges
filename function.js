ffcb4223671804eaa95ca719024819055bfa3500
mwerah <mohamed.werah@gmail.com>
2023-10-15 15:18:13 +0100

    added multiply function

---
+++ function.js
@@ -0,0 +1,11 @@
+/*
+write a simple JavaScript function called 'multipy' 
+that takes two numbers as params and returns 
+the result to the caller
+*/
+function multiply(num1, num2) {
+    let result = num1 * num2;
+    return result;
+
+}
+console.log(multiply(4,3))
