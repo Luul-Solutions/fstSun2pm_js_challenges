9deccbbfa3ea51335f24600cd39ab742e4dafe56
= <=>
2023-10-15 15:02:57 +0100

    added a function

---
+++ functions.js
@@ -0,0 +1,12 @@
+/*
+write a simple JavaScript function called 'multipy' 
+that takes two numbers as params and returns 
+the result to the caller
+*/
+function multiply(num1, num2) {
+    let result = num1 * num2
+    return result;
+}
+
+let sum = multiply(5,6)
+console.log(sum)
