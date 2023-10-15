c710bb460fd7ef29310761901fb4400ca31dcf5b
= <=>
2023-10-15 15:44:38 +0100

    added arrow function requirement

---
+++ arrowFunction.js
@@ -0,0 +1,13 @@
+// convert the variable function to an arrow function (fat arrow function)
+
+// let multiply = function (num1, num2) {
+//     let result = num1 * num2;
+//     return result
+// }
+
+let multiply = function (num1, num2) {
+        let result = num1 * num2;
+        return result
+    }
+
+console.log(multiply(9,8))
