86795fc366935a5ed0fb834866c7651e6d3f5ecd
= <user@users-MacBook-Pro.local>
2023-10-15 15:22:42 +0100

    saving my changes

---
+++ function.js
@@ -0,0 +1,14 @@
+/*
+write a simple Javascript function called 'multiply'
+that takes two numbersas parameters and returns the result
+to the caller
+*/
+
+function multiply(num1, num2){
+    let result = num1 * num2
+    return result
+}
+
+
+let sum = multiply(5,6)
+console.log(sum)
--- while.js
+++ while.js
@@ -1,5 +1,7 @@
-let count = 0;
-while (count < 20) {
-    console.log("Ali")
-    count++;
+// write a while loop that prints your surname 20 times
+ let counter = 10
+
+while(counter < 4){
+    console.log('Hassan')
+    counter++
 }
